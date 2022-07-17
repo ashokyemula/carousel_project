19a35ef5713d33ab6476064337f7f581506cafcb
ashokyemula <ashokyemula@gmail.com>
2022-07-17 09:28:37 -0400

    Carousel Project - first draft v0.1 July 17th

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CarouselExample from './CarouselExample';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>This is Carousel Implementation using Material UI</h2>
+
+      <div>
+        <CarouselExample/> 
+      </div>
+
+      <div>
+        <h2>This is Carousel Implementation with React Icons, not MUI</h2>
+          return <ImageSlider slides={SliderData} />;
+      </div>
+
     </div>
+    
   );
 }
 
